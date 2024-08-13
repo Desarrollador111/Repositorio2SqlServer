50bad69da2f2cf9d49f69ed4999043462bbd80bf
Gorostiola <goroztiola@gmail.com>
2024-08-13 09:13:56 -0600

    Carga de codigo a GitHub

---
+++ VersionSQL/SISTEMAS/bd1/dbo/Table/articulos.sql
@@ -0,0 +1,9 @@
+﻿/****** Object:  Table [dbo].[articulos]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE TABLE [dbo].[articulos](
+	[codigo] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](20) NULL,
+	[descripcion] [varchar](30) NULL,
+	[precio] [float] NULL,
+	[Fila1] [varchar](40) NULL
+) ON [PRIMARY]
